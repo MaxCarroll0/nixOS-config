5b53eae175a7ae40a12af2cbf29d90f1a8c3edab
Max <mjvcarroll@gmail.com>
2026-08-02 00:32:34 +0100

    Declare desktop s-tui settings

--- home/desktop.nix
+++ home/desktop.nix
@@ -18,6 +18,97 @@
   };
   xdg.configFile."btop/btop.conf".force = true;
 
+  xdg.configFile."s-tui/s-tui.conf" = {
+    force = true;
+    text = ''
+      [GraphControl]
+      refresh = 2.0
+      utf8 = True
+
+      [Temp,Graphs]
+      edge,0 = True
+      iwlwifi_1,0 = True
+      tctl,0 = True
+      composite,0 = True
+
+      [Frequency,Graphs]
+      avg = True
+      core 0 = True
+      core 1 = True
+      core 2 = True
+      core 3 = True
+      core 4 = True
+      core 5 = True
+      core 6 = True
+      core 7 = True
+      core 8 = True
+      core 9 = True
+      core 10 = True
+      core 11 = True
+
+      [Util,Graphs]
+      avg = True
+      core 0 = True
+      core 1 = True
+      core 2 = True
+      core 3 = True
+      core 4 = True
+      core 5 = True
+      core 6 = True
+      core 7 = True
+      core 8 = True
+      core 9 = True
+      core 10 = True
+      core 11 = True
+
+      [Power,Graphs]
+
+      [Fan,Graphs]
+      amdgpu,0 = True
+
+      [Temp,Summaries]
+      edge,0 = True
+      iwlwifi_1,0 = True
+      tctl,0 = True
+      composite,0 = True
+
+      [Frequency,Summaries]
+      avg = True
+      core 0 = True
+      core 1 = True
+      core 2 = True
+      core 3 = True
+      core 4 = True
+      core 5 = True
+      core 6 = True
+      core 7 = True
+      core 8 = True
+      core 9 = True
+      core 10 = True
+      core 11 = True
+
+      [Util,Summaries]
+      avg = True
+      core 0 = True
+      core 1 = True
+      core 2 = True
+      core 3 = True
+      core 4 = True
+      core 5 = True
+      core 6 = True
+      core 7 = True
+      core 8 = True
+      core 9 = True
+      core 10 = True
+      core 11 = True
+
+      [Power,Summaries]
+
+      [Fan,Summaries]
+      amdgpu,0 = True
+    '';
+  };
+
   xdg.configFile."powerdevilrc" = {
     force = true;
     text = ''
