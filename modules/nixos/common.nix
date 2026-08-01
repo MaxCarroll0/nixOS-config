2f8f4b14fb3ceaf8c183704731718ee5dcfe1bff
Max <mjvcarroll@gmail.com>
2026-08-01 18:07:34 +0100

    Use nix-community binary cache

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -116,12 +116,15 @@ in
     nix.settings.builders-use-substitutes = true;
     nix.settings.substituters = [
       "https://hyprland.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     nix.settings.trusted-substituters = [
       "https://hyprland.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     nix.settings.trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
     nix.settings.trusted-users = [
       "root"
